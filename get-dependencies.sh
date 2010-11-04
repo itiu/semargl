7f50308c19cca14906e776042e6c9f73f5385718
Itiu <ValeriyBushenev@gmail.com>
2010-11-04 17:24:05 +0300

    add zmq lib 2.0.10

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,6 +1,6 @@
 mongo_d_driver__hash=58ed682
 trioplax__hash=c9c8a8f
-zeromq__hash=bcecdef
+zeromq__hash=76c6a8b
 rabbitmq_connector__hash=02524e3
 rabbitmq_d__hash=9322e2e
 
@@ -17,23 +17,23 @@ mkdir build/src
 
 cd build
 
-wget http://github.com/itiu/mongo-d-driver/zipball/$mongo_d_driver__hash
+wget --no-check-certificate http://github.com/itiu/mongo-d-driver/zipball/$mongo_d_driver__hash
 unzip $mongo_d_driver__hash
 rm $mongo_d_driver__hash
 
-wget http://github.com/itiu/trioplax/zipball/$trioplax__hash
+wget --no-check-certificate http://github.com/itiu/trioplax/zipball/$trioplax__hash
 unzip $trioplax__hash
 rm $trioplax__hash
 
-wget http://github.com/itiu/zeromq-connector/zipball/$zeromq__hash
+wget --no-check-certificate http://github.com/itiu/zeromq-connector/zipball/$zeromq__hash
 unzip $zeromq__hash
 rm $zeromq__hash
 
-wget http://github.com/itiu/$rabbitmq_connector__project_name/zipball/$rabbitmq_connector__hash
+wget --no-check-certificate http://github.com/itiu/$rabbitmq_connector__project_name/zipball/$rabbitmq_connector__hash
 unzip $rabbitmq_connector__hash
 rm $rabbitmq_connector__hash
 
-wget http://github.com/selivanovm/$rabbitmq_d__project_name/zipball/$rabbitmq_d__hash
+wget --no-check-certificate http://github.com/selivanovm/$rabbitmq_d__project_name/zipball/$rabbitmq_d__hash
 unzip $rabbitmq_d__hash
 rm $rabbitmq_d__hash
 
